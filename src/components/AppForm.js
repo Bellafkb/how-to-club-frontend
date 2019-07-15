6830188b3bc2c872e2494d541b184dfde6331d86
bellafkb b <bellafkb@gmail.com>
2019-07-15 19:41:21 +0200

    changed feed

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -5,10 +5,10 @@ import { Form, Button, FormControl } from 'react-bootstrap'
 class AppForm extends Component {
     render() {
         return (
-            <div class="form-container">
+            <div class="form-container" >
                 <Form inline >
-                    <FormControl type="text" placeholder="Search"/>
-                    <Button variant="outline-info">Search</Button>
+                    <FormControl style={{width:'80%'}}  type="text" placeholder="Search"/>
+                    <Button style={{width:'20%'}} variant="outline-info">Search</Button>
                 </Form>
             </div>
         );

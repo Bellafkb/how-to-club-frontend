7d6c2faff5388f838cf435bdb658edfde3d8362e
bellafkb b <bellafkb@gmail.com>
2019-07-16 11:27:25 +0200

    autocomplete

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Form, Button, FormControl } from 'react-bootstrap'
-
+import Autocomplete from 'react-google-autocomplete'
 
 class AppForm extends Component {
     render() {
         return (
             <div class="form-container" >
                 <Form inline >
-                    <FormControl style={{width:'80%'}}  type="text" placeholder="Search"/>
+                <Autocomplete
+                class="autocomplete"
+                        style={{ width: '80%' }}
+                        onPlaceSelected={(place) => {
+                            console.log(place);
+                        }}
+                        types={['(cities)']}
+                        componentRestrictions={{ country: "de" }}
+                    />
                     <Button style={{width:'20%'}} variant="outline-info">Search</Button>
                 </Form>
             </div>
